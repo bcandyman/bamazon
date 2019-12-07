c5f8a3f04320cdcc4e5cf13844d2949bee0b1a46
Benjamin Candy <bcandyman93@gmail.com>
2019-12-07 09:05:30 -0800

    Added code to connect to database "bamazon_db".

---
+++ index.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql")
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "sqlPassword",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+
+    console.log("connected as id ", connection.threadId);
+
+
+    connection.end()
+})
