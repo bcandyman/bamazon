2fbaa27f4511db5c825f62c65b4ef8fd304497ee
Benjamin Candy <bcandyman93@gmail.com>
2019-12-11 22:43:18 -0800

    Added notes and error checking

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const mysql = require("mysql")
 const inquirer = require("inquirer");
 
 
+
+//contructor to create objects used for inquirer
 class InqUserPrompt {
     constructor(name, type, message, choices) {
         this.name = name;
@@ -13,23 +15,24 @@ class InqUserPrompt {
         }
 
         if (type === "number") {
-            this.validate = function (value) {
-                if (value === "") {
+            this.validate = function (val) {
+                if (val === "") {
                     return "Please enter the ID of the item you would like to purchase"
                 }
                 return true
             };
-            this.filter = function (value) {
-                if (isNaN(value)) {
+            this.filter = function (val) {
+                if ((isNaN(val)) || (val <= 0)) {
                     return ""
                 }
-                return value
+                return val
             }
         }
     }
 }
 
 
+//add parameters for sql database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -39,104 +42,150 @@ const connection = mysql.createConnection({
 });
 
 
+//connect to sql database
+connection.connect(function (err) {
+    if (err) {
+        throw err
+    };
+
+    //display available inventory
+    displayInventory();
+})
 
 
-
+//this function prompts user for available commands
 runSearch = () => {
     promptUser = (prompt) => {
-        inquirer.prompt(prompt).then(function (answer) {
-            switch (answer.action) {
+        inquirer.prompt(prompt).then(function (ans) {
+            switch (ans.action) {
                 case "Make a purchase":
                     purchase();
                     break;
                 case "View inventory":
                     displayInventory();
                     break;
+                case "exit":
+                    connection.end()
+                    return
             }
         });
     }
-
+    //create display object for inquirer
     const choices = ["View inventory", "Make a purchase", "exit"]
     const userPrompt = new InqUserPrompt("action", "list", "What would you like to do?", choices);
+    //call prompt user inquire
     promptUser({ ...userPrompt })
-
 }
 
 
 
 
-
-displayInventory = () => {
-    connection.query("SELECT * FROM products INNER JOIN departments ON products.department_id = departments.department_id", (err, response) => {
-
+//this function displays available items within the sql database
+function displayInventory() {
+    //get all items from sql database
+    connection.query("SELECT * FROM products INNER JOIN departments ON products.department_id = departments.department_id", (err, res) => {
         if (err) {
-            throw err;
+            reject(err);
         };
 
+        //this method created consistent string length and is used to ensure proper inventory display
+        //txt = desired text to return
+        //len = desired length of return string
         fixLen = (txt, len) => {
             const spcCount = len - txt.toString().length
             return txt + " ".repeat(spcCount)
         }
 
+        //display table header for inventory table
         console.log(fixLen("ID", 4) + "|" + fixLen("Product", 20) + "|" + fixLen("Department", 15) + "|" + fixLen("Price", 10) + "|" + fixLen("Qty", 6))
-
-        for (let i = 0; i < response.length; i++) {
-            let item = response[i]
+        
+        //loop through each item in the database
+        for (let i = 0; i < res.length; i++) {
+            let item = res[i]
+            //display item and contents
             console.log(fixLen(item.item_id, 4) + "|" + fixLen(item.product_name, 20) + "|" + fixLen(item.department_name, 15) + "|" + fixLen(item.price, 10) + "|" + fixLen(item.stock_quantity, 6));
         }
         console.log("-----------------------------------");
+
+        //display user commands
+        runSearch()
     });
-    connection.end();
 }
+
 
 
 
 
 purchase = () => {
     promptUser = (prompt) => {
-        inquirer.prompt(prompt).then(function (answer) {
+        inquirer.prompt(prompt).then(function (ans) {
 
-            const qtySold = answer.qty
+            let qtySold = ans.qty
 
-            connection.query("SELECT * FROM products WHERE item_id = " + answer.item, function (err, response) {
+            connection.query("SELECT * FROM products WHERE item_id = " + ans.item, function (err, res) {
 
                 if (err) {
                     console.log(err)
                 }
 
-                const item = response[0]
-                currQty = response[0].stock_quantity
+                const item = res[0]
+                currQty = res[0].stock_quantity
 
+
+                // if order cannot be fulfilled
                 if (item.stock_quantity - qtySold < 0) {
+                    //display to user order of item cannot be fulfilled
                     console.log("Sorry, we don't have that many " + item.product_name + "s");
+
+
+                    //create prompt object to ask if user want to purchase available stock
+                    const userPrompt = new InqUserPrompt("purchaseRemaining", "confirm", "We only have " + item.stock_quantity + ". Would you like to purchase our remaining items?");
+                    //promput user
+                    inquirer.prompt({ ...userPrompt }).then(function (resp) {
+                        //if user wants to purchase remaining stock
+                        if (resp.purchaseRemaining) {
+                            //adjust requested quantity to available stock
+                            qtySold = item.stock_quantity
+                            //update sql database
+                            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + qtySold + " WHERE item_id = " + ans.item, function (err, res) {
+                                displayInventory()
+                            })
+                        }
+
+                        //if user doesn't want to purchase ramaining stock
+                        else {
+                            displayInventory()
+                        }
+                    })
                 }
 
+                //if order request can be fulfilled
                 else {
-                    connection.query("UPDATE products SET stock_quantity = stock_quantity - " + qtySold + " WHERE item_id = 1", function (err, response) {
+                    //update sql database
+                    connection.query("UPDATE products SET stock_quantity = stock_quantity - " + qtySold + " WHERE item_id = " + ans.item, function (err, res) {
 
                         if (err) {
                             console.log(err)
                         }
 
+                        //display order details
                         console.log("You owe $" + item.price * qtySold + " plus tax!");
                         console.log("Thank you!!");
+
+                        //keep order details focused before displaying inventory
+                        setTimeout(() => {
+                            displayInventory()
+                        }, 5000);
                     })
                 }
-                connection.end()
             });
         })
     };
 
+    //create user prompt object
     const userItemPrompt = new InqUserPrompt("item", "number", "What item would you like to purchase?");
     const userQtyPrompt = new InqUserPrompt("qty", "number", "How many?");
     const userPrompt = [{ ...userItemPrompt }, { ...userQtyPrompt }];
-
+    //prompt user
     promptUser(userPrompt)
 }
-
-
-
-
-
-
-runSearch()
