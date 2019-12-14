e9dff4acd532b739828b250bc88937b296638d4d
Benjamin Candy <bcandyman93@gmail.com>
2019-12-13 20:33:32 -0800

    Changed to return an object from a pre-built template rather than creating an object using a constructor

--- utilities/inquirerPrompt.js
+++ utilities/inquirerPrompt.js
@@ -1,30 +1,30 @@
 //contructor to create objects used for inquirer
-class InqUserPrompt {
-     constructor(name, type, message, choices) {
-        this.name = name;
-        this.type = type;
-        this.message = message;
-
-        if (type === "list") {
-            this.choices = choices;
-        }
-
-        if (type === "number") {
-            this.validate = function (val) {
+const inqPrompt = (name, type, message, choices) => {
+    const inqPrompt = {
+        name: name,
+        type: type,
+        message: message,
+    }
+    switch (type) {
+        case "list":
+                inqPrompt.choices = choices
+            break
+        case "number":
+                inqPrompt.validate = function (val) {
                 if (val === "") {
                     return "Please enter the ID of the item you would like to purchase"
                 }
                 return true
             };
-            this.filter = function (val) {
+            inqPrompt.filter = function (val) {
                 if ((isNaN(val)) || (val <= 0)) {
                     return ""
                 }
                 return val
             }
-        }
     }
+    return inqPrompt
 }
 
-// Exporting the InqUserPrompt constructor which we will use for inquirer
-module.exports = InqUserPrompt;
+// Export the inqPrompt which will be used for inquirer
+module.exports = inqPrompt;
